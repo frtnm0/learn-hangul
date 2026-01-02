1ef1ece427175f9ac74ff1de1ce1ff9a3b239d53
frtnm01 <fortenem01@gmail.com>
2026-01-02 18:30:52 +0800

    Set base and build.outDir for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/learn-hangul/',
   plugins: [react()],
   server: {
     host: true,
     port: 5173
+  },
+  build: {
+    outDir: 'docs'
   }
 })
